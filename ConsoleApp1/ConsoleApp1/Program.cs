c52bf6bdf28ca0b81cf05cd979ba2e50404c9fb6
rhenninger61 <143552435+rhenninger61@users.noreply.github.com>
2024-12-05 15:15:09 -0500

    DoMath() added

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,6 +9,13 @@
 
             Console.WriteLine("Enter your second number: ");
             int y = int.Parse(Console.ReadLine()!);
+
+            DoMath(x, y);
+        }
+        public static void DoMath(int x, int y)
+        {
+            int z = x * y;
+            Console.WriteLine("Result: " + z);
         }
     }
 }
